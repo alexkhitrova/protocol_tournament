f3f8afd085afde2f97e9d36526d4758fad3a62bc
alexkhitrova <alexkhitrova@yandex.ru>
2020-11-08 21:09:59 +0300

    reg exp

--- protocol.py
+++ protocol.py
@@ -5,6 +5,194 @@ from PyQt5.QtGui import QFont, QRegExpValidator
 from PyQt5.QtCore import QCoreApplication, QRegExp
 
 
+class Registration(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(300, 100, 1000, 400)
+        self.setWindowTitle('Регистрация')
+
+        self.team_exists = QLabel(self)
+        self.team_exists.setText('<b style="color: rgb(250, 55, 55);">Такая команда уже зарегистрирована</b>')
+        self.team_exists.move(370, 350)
+        self.team_exists.hide()
+        self.name_space = QLabel(self)
+        self.name_space.setText('<b style="color: rgb(250, 55, 55);">Введите название команды</b>')
+        self.name_space.move(370, 350)
+        self.name_space.hide()
+        self.noname_person = QLabel(self)
+        self.noname_person.setText('<b style="color: rgb(250, 55, 55);">Введите имя участника</b>')
+        self.noname_person.move(370, 350)
+        self.noname_person.hide()
+        self.invalid_class = QLabel(self)
+        self.invalid_class.setText('<b style="color: rgb(250, 55, 55);">Некорректный номер класса</b>')
+        self.invalid_class.move(370, 350)
+        self.invalid_class.hide()
+
+        self.name = QLabel(self)
+        self.name.setText('<b>Название команды</b>')
+        self.name.move(50, 50)
+        self.name_input = QLineEdit(self)
+        self.name_input.move(250, 50)
+        self.name_input.textChanged.connect(self.check)
+        rx = QRegExp('[\S][\S\s]*')
+        validator = QRegExpValidator(rx, self)
+        self.name_input.setValidator(validator)
+        rx_names = QRegExp('[\D]*')
+        validator_names = QRegExpValidator(rx_names, self)
+        rx_nums = QRegExp('[\d]*')
+        validator_nums = QRegExpValidator(rx_nums, self)
+
+        self.people = QLabel(self)
+        self.people.setText('<b>Состав команды</b>')
+        self.people.move(50, 100)
+
+        self.one = QLabel(self)
+        self.one.setText('1: Фамилия, имя')
+        self.one.move(250, 100)
+        self.one_input = QLineEdit(self)
+        self.one_input.move(380, 98)
+        self.one_input.setValidator(validator_names)
+        self.one_input.textChanged.connect(self.check)
+        self.two = QLabel(self)
+        self.two.setText('2: Фамилия, имя')
+        self.two.move(250, 150)
+        self.two_input = QLineEdit(self)
+        self.two_input.move(380, 148)
+        self.two_input.setValidator(validator_names)
+        self.two_input.textChanged.connect(self.check)
+        self.three = QLabel(self)
+        self.three.setText('3: Фамилия, имя')
+        self.three.move(250, 200)
+        self.three_input = QLineEdit(self)
+        self.three_input.move(380, 198)
+        self.three_input.setValidator(validator_names)
+        self.three_input.textChanged.connect(self.check)
+        self.four = QLabel(self)
+        self.four.setText('4: Фамилия, имя')
+        self.four.move(250, 250)
+        self.four_input = QLineEdit(self)
+        self.four_input.move(380, 248)
+        self.four_input.setValidator(validator_names)
+        self.four_input.textChanged.connect(self.check)
+
+        self.one_class = QLabel(self)
+        self.one_class.setText('Класс')
+        self.one_class.move(600, 100)
+        self.one_class_input = QLineEdit(self)
+        self.one_class_input.move(650, 98)
+        self.one_class_input.textChanged.connect(self.check)
+        self.one_class_input.setValidator(validator_nums)
+        self.two_class = QLabel(self)
+        self.two_class.setText('Класс')
+        self.two_class.move(600, 150)
+        self.two_class_input = QLineEdit(self)
+        self.two_class_input.move(650, 148)
+        self.two_class_input.textChanged.connect(self.check)
+        self.two_class_input.setValidator(validator_nums)
+        self.three_class = QLabel(self)
+        self.three_class.setText('Класс')
+        self.three_class.move(600, 200)
+        self.three_class_input = QLineEdit(self)
+        self.three_class_input.move(650, 198)
+        self.three_class_input.textChanged.connect(self.check)
+        self.three_class_input.setValidator(validator_nums)
+        self.four_class = QLabel(self)
+        self.four_class.setText('Класс')
+        self.four_class.move(600, 250)
+        self.four_class_input = QLineEdit(self)
+        self.four_class_input.move(650, 248)
+        self.four_class_input.textChanged.connect(self.check)
+        self.four_class_input.setValidator(validator_nums)
+
+        self.ok = QPushButton('OK', self)
+        self.ok.clicked.connect(self.team_people)
+        self.ok.move(50, 350)
+
+        self.ready = QPushButton('READY', self)
+        self.ready.clicked.connect(self.finish_reg)
+        self.ready.move(850, 350)
+
+    def send(self):
+        for i in list(teams_people.keys()):
+            teams.append(i)
+
+    def give_in(self):
+        give.show()
+
+    def team_people(self):
+        teams_people[self.name_input.text()] = ((self.one_input.text(), self.one_class_input.text()),
+                                                (self.two_input.text(), self.two_class_input.text()),
+                                                (self.three_input.text(), self.three_class_input.text()),
+                                                (self.four_input.text(), self.four_class_input.text()))
+        people = [self.one_input.text(), self.two_input.text(), self.three_input.text(), self.four_input.text()]
+        classes = [self.one_class_input.text(), self.two_class_input.text(), self.three_class_input.text(),
+                   self.four_class_input.text()]
+        if '-' in people:
+            people.remove('-')
+        if '-' in classes:
+            classes.remove('-')
+        register_team(self.name_input.text(), ', '.join(people), ', '.join(classes), max(classes))
+
+        self.name_input.setText('')
+        self.one_input.setText('')
+        self.one_class_input.setText('')
+        self.two_input.setText('')
+        self.two_class_input.setText('')
+        self.three_input.setText('')
+        self.three_class_input.setText('')
+        self.four_input.setText('')
+        self.four_class_input.setText('')
+
+    def check(self):
+        self.name_space.hide()
+        self.team_exists.hide()
+        self.noname_person.hide()
+        self.invalid_class.hide()
+        if self.name_input.text() == '':
+            self.ok.setEnabled(False)
+            self.name_space.show()
+        elif self.name_input.text() in teams_people.keys():
+            self.ok.setEnabled(False)
+            self.team_exists.show()
+        elif self.one_input.text() == '' or self.two_input.text() == '' \
+                or self.three_input.text() == '' or self.four_input.text() == '':
+            self.ok.setEnabled(False)
+            self.noname_person.show()
+        elif self.one_class_input.text() not in possible_classes or \
+                self.two_class_input.text() not in possible_classes or \
+                self.three_class_input.text() not in possible_classes or \
+                self.four_class_input.text() not in possible_classes:
+            self.ok.setEnabled(False)
+            self.invalid_class.show()
+        else:
+            self.ok.setEnabled(True)
+            self.team_exists.hide()
+            self.invalid_class.hide()
+            self.name_space.hide()
+
+    def closeEvent(self, event):
+        reply = QMessageBox.question(self, 'Message', "Вы точно хотите выйти?",
+                                     QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+
+        if reply == QMessageBox.Yes:
+            event.accept()
+        else:
+            event.ignore()
+
+    def finish_reg(self, event):
+        reply = QMessageBox.question(self, 'Message', "Вы точно хотите завершить регистрацию?\n"
+                                                      "Количество команд: " + str(len(teams_people.keys())),
+                                     QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+
+        if reply == QMessageBox.Yes:
+            self.send()
+            self.give_in()
+            self.hide()
+
+
 class GiveIn(QWidget):
     def __init__(self):
         super().__init__()
@@ -47,8 +235,8 @@ class GiveIn(QWidget):
         self.name_input = QLineEdit(self)
         self.name_input.move(250, 48)
         self.name_input.textChanged.connect(self.check_team)
-        rx_name = QRegExp('[\S][\S\s]*')
-        validator = QRegExpValidator(rx_name, self)
+        rx = QRegExp('[\S][\S\s]*')
+        validator = QRegExpValidator(rx, self)
         self.name_input.setValidator(validator)
 
         self.num = QLabel(self)
@@ -157,198 +345,13 @@ class GiveIn(QWidget):
         else:
             event.ignore()
 
-class Registration(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.initUI()
-
-    def initUI(self):
-        self.setGeometry(300, 100, 1000, 400)
-        self.setWindowTitle('Регистрация')
-
-        self.team_exists = QLabel(self)
-        self.team_exists.setText('<b style="color: rgb(250, 55, 55);">Такая команда уже зарегистрирована</b>')
-        self.team_exists.move(370, 350)
-        self.team_exists.hide()
-        self.name_space = QLabel(self)
-        self.name_space.setText('<b style="color: rgb(250, 55, 55);">Введите название команды</b>')
-        self.name_space.move(370, 350)
-        self.name_space.hide()
-        self.noname_person = QLabel(self)
-        self.noname_person.setText('<b style="color: rgb(250, 55, 55);">Введите имя участника</b>')
-        self.noname_person.move(370, 350)
-        self.noname_person.hide()
-        self.invalid_class = QLabel(self)
-        self.invalid_class.setText('<b style="color: rgb(250, 55, 55);">Некорректный номер класса</b>')
-        self.invalid_class.move(370, 350)
-        self.invalid_class.hide()
-
-        self.name = QLabel(self)
-        self.name.setText('<b>Название команды</b>')
-        self.name.move(50, 50)
-        self.name_input = QLineEdit(self)
-        self.name_input.move(250, 50)
-        self.name_input.textChanged.connect(self.check)
-        rx = QRegExp('[\S][\S\s]*')
-        validator = QRegExpValidator(rx, self)
-        self.name_input.setValidator(validator)
-
-        self.people = QLabel(self)
-        self.people.setText('<b>Состав команды</b>')
-        self.people.move(50, 100)
-
-        self.one = QLabel(self)
-        self.one.setText('1: Фамилия, имя')
-        self.one.move(250, 100)
-        self.one_input = QLineEdit(self)
-        self.one_input.move(380, 98)
-        self.one_input.setValidator(validator)
-        self.one_input.textChanged.connect(self.check)
-        self.two = QLabel(self)
-        self.two.setText('2: Фамилия, имя')
-        self.two.move(250, 150)
-        self.two_input = QLineEdit(self)
-        self.two_input.move(380, 148)
-        self.two_input.setValidator(validator)
-        self.two_input.textChanged.connect(self.check)
-        self.three = QLabel(self)
-        self.three.setText('3: Фамилия, имя')
-        self.three.move(250, 200)
-        self.three_input = QLineEdit(self)
-        self.three_input.move(380, 198)
-        self.three_input.setValidator(validator)
-        self.three_input.textChanged.connect(self.check)
-        self.four = QLabel(self)
-        self.four.setText('4: Фамилия, имя')
-        self.four.move(250, 250)
-        self.four_input = QLineEdit(self)
-        self.four_input.move(380, 248)
-        self.four_input.setValidator(validator)
-        self.four_input.textChanged.connect(self.check)
-
-        self.one_class = QLabel(self)
-        self.one_class.setText('Класс')
-        self.one_class.move(600, 100)
-        self.one_class_input = QLineEdit(self)
-        self.one_class_input.move(650, 98)
-        self.one_class_input.textChanged.connect(self.check)
-        self.one_class_input.setValidator(validator)
-        self.two_class = QLabel(self)
-        self.two_class.setText('Класс')
-        self.two_class.move(600, 150)
-        self.two_class_input = QLineEdit(self)
-        self.two_class_input.move(650, 148)
-        self.two_class_input.textChanged.connect(self.check)
-        self.two_class_input.setValidator(validator)
-        self.three_class = QLabel(self)
-        self.three_class.setText('Класс')
-        self.three_class.move(600, 200)
-        self.three_class_input = QLineEdit(self)
-        self.three_class_input.move(650, 198)
-        self.three_class_input.textChanged.connect(self.check)
-        self.three_class_input.setValidator(validator)
-        self.four_class = QLabel(self)
-        self.four_class.setText('Класс')
-        self.four_class.move(600, 250)
-        self.four_class_input = QLineEdit(self)
-        self.four_class_input.move(650, 248)
-        self.four_class_input.textChanged.connect(self.check)
-        self.four_class_input.setValidator(validator)
-
-        self.ok = QPushButton('OK', self)
-        self.ok.clicked.connect(self.team_people)
-        self.ok.move(50, 350)
-
-        self.ready = QPushButton('READY', self)
-        self.ready.clicked.connect(self.finish_reg)
-        self.ready.move(850, 350)
-
-    def send(self):
-        for i in list(teams_people.keys()):
-            teams.append(i)
-
-    def give_in(self):
-        give.show()
-
-    def team_people(self):
-        teams_people[self.name_input.text()] = ((self.one_input.text(), self.one_class_input.text()),
-                                                (self.two_input.text(), self.two_class_input.text()),
-                                                (self.three_input.text(), self.three_class_input.text()),
-                                                (self.four_input.text(), self.four_class_input.text()))
-        people = [self.one_input.text(), self.two_input.text(), self.three_input.text(), self.four_input.text()]
-        classes = [self.one_class_input.text(), self.two_class_input.text(), self.three_class_input.text(),
-                   self.four_class_input.text()]
-        if '-' in people:
-            people.remove('-')
-        if '-' in classes:
-            classes.remove('-')
-        register_team(self.name_input.text(), ', '.join(people), ', '.join(classes), max(classes))
-
-        self.name_input.setText('')
-        self.one_input.setText('')
-        self.one_class_input.setText('')
-        self.two_input.setText('')
-        self.two_class_input.setText('')
-        self.three_input.setText('')
-        self.three_class_input.setText('')
-        self.four_input.setText('')
-        self.four_class_input.setText('')
-
-    def check(self):
-        self.name_space.hide()
-        self.team_exists.hide()
-        self.noname_person.hide()
-        self.invalid_class.hide()
-        if self.name_input.text() == '':
-            self.ok.setEnabled(False)
-            self.name_space.show()
-        elif self.name_input.text() in teams_people.keys():
-            self.ok.setEnabled(False)
-            self.team_exists.show()
-        elif self.one_input.text() == '' or self.two_input.text() == '' \
-                or self.three_input.text() == '' or self.four_input.text() == '':
-            self.ok.setEnabled(False)
-            self.noname_person.show()
-        elif self.one_class_input.text() not in possible_classes or \
-                self.two_class_input.text() not in possible_classes or \
-                self.three_class_input.text() not in possible_classes or \
-                self.four_class_input.text() not in possible_classes:
-            self.ok.setEnabled(False)
-            self.invalid_class.show()
-        else:
-            self.ok.setEnabled(True)
-            self.team_exists.hide()
-            self.invalid_class.hide()
-            self.name_space.hide()
-
-    def closeEvent(self, event):
-        reply = QMessageBox.question(self, 'Message', "Вы точно хотите выйти?",
-                                     QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
-
-        if reply == QMessageBox.Yes:
-            event.accept()
-        else:
-            event.ignore()
-
-    def finish_reg(self, event):
-        reply = QMessageBox.question(self, 'Message', "Вы точно хотите завершить регистрацию?\n"
-                                                      "Количество команд: " + str(len(teams_people.keys())),
-                                     QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
-
-        if reply == QMessageBox.Yes:
-            self.send()
-            self.give_in()
-            self.hide()
 
 teams_people = {}
-teams_problems = {}
 solved = {}
-possible_classes = ('5', '6', '7', '8', '9', '-')
 teams = []
 points = [1, 1, 2, 2, 3, 3]
-name_error = False
-noname_error = False
-class_error = False
+possible_classes = ('5', '6', '7', '8', '9', '-')
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
