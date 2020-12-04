b7119bc1e85d19a0fe6d4c836d580089024ba93c
UARTman <uartman@mail.ru>
2020-12-04 17:42:17 +0300

    Change activator.py + add build instructions
    Look for instructions in BUILDING.MD

--- archivator.py
+++ archivator.py
@@ -1,7 +1,11 @@
 import PyInstaller.__main__
 
-PyInstaller.__main__.run([
-    "--paths=C:/Users/Alexandra/AppData/Local/Programs/Python/Python36/Lib/site-packages",
-    '--onefile', '--windowed', 'protocol.py'
 
-])
+PyInstaller.__main__.run(
+    [
+        # "--paths=C:/Users/Alexandra/AppData/Local/Programs/Python/Python36/Lib/site-packages",
+        # "--onefile",
+        # "--windowed",
+        "protocol.py",
+    ]
+)
